2f3e2a0d21dbf8384230eb65c41d1ae89c06d376
Anastasia9595 <Anastasia9595@googlemail.com>
2026-08-25 12:44:46 +0200

    feat: Implement barcode scanner interface and its fake/mobile implementations

---
+++ lib/repository/mobile_scanner_adapter_repository.dart
@@ -0,0 +1,35 @@
+import 'package:mobile_scanner/mobile_scanner.dart';
+import '../interface/barcode_scanner_interface.dart';
+import 'dart:async';
+
+class MobileScannerAdapter implements BarcodeScannerInterface {
+  final _controller = MobileScannerController();
+  final _out = StreamController<String>.broadcast();
+  StreamSubscription<BarcodeCapture>? _sub;
+
+  Future<void> stop() async {
+    await _sub?.cancel();
+    await _controller.stop();
+  }
+
+  @override
+  Future<String> get scan => _out.stream.first;
+
+  @override
+  Future<void> startScan() async {
+    await _controller.start();
+    _sub = _controller.barcodes.listen((capture) {
+      for (final barcode in capture.barcodes) {
+        if (barcode.rawValue != null) {
+          _out.add(barcode.rawValue!);
+        }
+      }
+    });
+  }
+
+  @override
+  Future<void> stopScan() async {
+    await _sub?.cancel();
+    await _controller.stop();
+  }
+}
