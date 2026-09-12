5aa6b5bc06c497f35f6eaa5ebed4cd8db02ef5c1
Anastasia9595 <Anastasia9595@googlemail.com>
2026-09-12 09:31:00 +0200

    feat: Update analysis options and environment configurations for widgetbook

--- lib/interface/image_picker_interface.dart
+++ lib/interface/image_picker_interface.dart
@@ -1,8 +1,7 @@
-import 'package:image_picker/image_picker.dart';
 import '../enums/image_source_option.dart';
 
 /// Kapselt den Zugriff auf Kamera und Galerie, damit die UI nicht direkt
 /// vom Plugin abhaengt und in Tests ersetzt werden kann.
 abstract class ImagePickerService {
-  Future<XFile?> pickImage(ImageSourceOption source);
+  Future<dynamic> pickImage(ImageSourceOption source);
 }
